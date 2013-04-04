78cfef12e84f86824c91ed0e8ce2e99176e2dd41
mktonerm@gmail.com <mktonerm@gmail.com>
2013-04-04 17:38:55 -0500

    ...

--- TopLineApp/scripts/topLineLocalStorage.js
+++ TopLineApp/scripts/topLineLocalStorage.js
@@ -1,65 +1,79 @@
+function id(element) {
+	return document.getElementById(element);
+}
+ 
 document.addEventListener("deviceready", onDeviceReady, false);
-
+ 
 function onDeviceReady() {
-    navigator.splashscreen.hide();
-	localStorageApp = new localStorageApp();
-	localStorageApp.run();
+	geolocationApp = new geolocationApp();
+	geolocationApp.run();
+    
+}
+ 
+function geolocationApp() {
 }
 
-function localStorageApp() {
-}
-
-localStorageApp.prototype = {
+geolocationApp.prototype = {
+	_watchID:null,
+    
 	run:function() {
 		var that = this;
-		document.getElementById("insertVariable").addEventListener("click", function() {
-			that._insertVariable.apply(that, arguments);
-		});
-		document.getElementById("searchVariable").addEventListener("click", function() {
-			that._getVariable.apply(that, arguments);
-		});
-		document.getElementById("clearLocalStorage").addEventListener("click", function() {
-			that._clearLocalStorage.apply(that, arguments);
-		});
-		document.getElementById("removeVariable").addEventListener("click", function() {
-			that._removeVariable.apply(that, arguments);
-		});
+		document.getElementById("lerCoordenadas").addEventListener("click", function() {
+			that._handleWatch.apply(that, arguments);
+		}, false);
 	},
     
-    
-	_insertVariable:function() {
-		var variableNameInput = document.getElementById("variableNameInput"),
-		valueInput = document.getElementById("valueInput");
-        
-		localStorage.setItem(variableNameInput.value, valueInput.value);
-		variableNameInput.value = "";
-		valueInput.value = "";
+	_handleRefresh:function() {
+		var options = {
+			enableHighAccuracy: true
+		},
+		that = this;
+		navigator.geolocation.getCurrentPosition(function() {
+			that._onSuccess.apply(that, arguments);
+		}, function() {
+			that._onError.apply(that, arguments);
+		}, options);
 	},
     
-	_getVariable:function() {
-		var getRemoveVariableNameInput = document.getElementById("getRemoveVariableNameInput"),
-		result = document.getElementById("result");
-		if (localStorage.getItem(getRemoveVariableNameInput.value) != undefined) {
-			result.value = localStorage.getItem(getRemoveVariableNameInput.value);
+	_handleWatch:function() {
+		var that = this;
+		// If watch is running, clear it now. Otherwise, start it.
+		if (that._watchID != null) {
+			that._setResults();
+			navigator.geolocation.clearWatch(that._watchID);
+			that._watchID = null;
 		}
 		else {
-			result.value = "No such record!"
+			// Update the watch every second.
+			var options = {
+				frequency: 1000,
+				enableHighAccuracy: true
+			};
+			that._watchID = navigator.geolocation.watchPosition(function() {
+				that._onSuccess.apply(that, arguments);
+			}, function() {
+				that._onError.apply(that, arguments);
+			}, options);
 		}
 	},
     
-	_removeVariable:function() {
-		var searchRemoveNameInput = document.getElementById("getRemoveVariableNameInput"),
-		result = document.getElementById("result");
-		if (localStorage.getItem(searchRemoveNameInput.value) != undefined) {
-			localStorage.removeItem(searchRemoveNameInput.value);
-			result.value = "Deleted";
+	_onSuccess:function(position) {
+		// Successfully retrieved the geolocation information. Display it all.
+        viewModel.lojaSelecionada.set("LojLatitude", position.coords.latitude);
+        viewModel.lojaSelecionada.set("LojLongitude", position.coords.longitude);	
+	},
+    
+	_onError:function(error) {
+		this._setResults('code: ' + error.code + '<br/>' +
+						 'message: ' + error.message + '<br/>');
+	},
+    
+	_setResults:function(value) {
+		if (!value) {
+			document.getElementById("results").innerHTML = "";
 		}
 		else {
-			result.value = "No such record!";
+			document.getElementById("results").innerHTML = value;
 		}
 	},
-    
-	_clearLocalStorage:function() {
-		localStorage.clear();
-	}
 }

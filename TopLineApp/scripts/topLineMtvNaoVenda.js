6418747671480f343ae0d0f926d7acf1fd5b503d
mktonerm@gmail.com <mktonerm@gmail.com>
2013-05-05 16:29:09 -0500

    ...

--- TopLineApp/scripts/topLineMtvNaoVenda.js
+++ TopLineApp/scripts/topLineMtvNaoVenda.js
@@ -2,7 +2,6 @@
 	var baseUrl = "http://www.revenuemachine.com.br/mobile/api";
 	//var baseUrl = "http://localhost:50000/api";
 
-
 	//Schema Motivos não venda
 	var scMotivosNaoVenda = { 
 		model: {
@@ -46,9 +45,7 @@
 		},
 		batch: true,
 		schema: scMotivosNaoVenda,
-		change: function(e) {
-            var motivoId = this.view()[0].get("MnvId");
-            viewModelNaoVenda.motivoNaoVenda.set("MnvId", motivoId);            
+		change: function(e) {         
 			viewModelNaoVenda.set("motivos", this.view());   
 		}        
         
@@ -76,69 +73,66 @@
 	var viewModelNaoVenda = kendo.observable({		
 		dsMotivosNaoVenda: dsMotivosNaoVenda,
 		dsNaoVenda: dsNaoVenda,
-		motivosNaoVenda: motivosNaoVenda,
 		motivoNaoVenda: {},        
 		motivos: [],
 		motivo: [],
 		vendedorSelecionado: {},
-		salvarNaoVenda: salvarNaoVenda,
-		cancelarNaoVenda: cancelarNaoVenda
-        //,confirma: false
+		naoVendaViewInit: naoVendaViewInit,
+		naoVendaViewShow: naoVendaViewShow
 	});
 
-	function salvarNaoVenda() {
-        /*
-		var that = this;
-		navigator.notification.confirm('Confirma Não Venda?', 
-									   function() {                                               
-										   onConfirmNaoVenda.apply(that, arguments);
-									   }, 
-									   'Atendimento', 
-									   'Não,Sim'
-		); 
+	function naoVendaViewInit(e) {
+		var view = e.view;
+        
+		view.element.find("#salvarMaior").data("kendoMobileButton").bind("click", function() {	
+			viewModelNaoVenda.dsNaoVenda.one("change", function() {	
+				//Marca o checkbox como default
+				document.getElementById("chkVendeu").checked = "checked";
+				view.loader.hide();				
+				app.navigate("#dentroFila-view"); 
+			});
+      
+             console.log("View: " , viewModelNaoVenda);
             
-		//Caso não confirme a gravação retorna para a tela de edição!
-		if (!viewModelNaoVenda.get("confirma")) {							
-			return;
-		}
-        */
+			view.loader.show();
+			viewModelNaoVenda.dsNaoVenda.sync();
+		});
+        
+		view.element.find("#cancelarMaior").data("kendoMobileButton").bind("click", function(e) {
+			e.preventDefault();
+			viewModelNaoVenda.dsNaoVenda.one("change", function() {
+				document.getElementById("chkVendeu").checked = "checked";
+				view.loader.hide();
+				app.navigate("#dentroFila-view");
+			});
+
+			view.loader.show();
+			viewModelNaoVenda.dsNaoVenda.cancelChanges();			
+		});
+	}
+    
+	function naoVendaViewShow() {
 		var RLoId = viewModel.vendedorSelecionado.get("RedeLojId");
 		var LcoId = viewModel.vendedorSelecionado.get("LojaColId");
 		var LojId = viewModel.vendedorSelecionado.get("LojId");
-                
+          
+		var naoVenda = viewModelNaoVenda.dsNaoVenda.add(); 
+        
+        console.log("Nao Venda: " , naoVenda);
+        
+		viewModelNaoVenda.set("motivoNaoVenda", naoVenda); 
 		viewModelNaoVenda.motivoNaoVenda.set("RLojId", RLoId);
 		viewModelNaoVenda.motivoNaoVenda.set("LcoId", LcoId);
 		viewModelNaoVenda.motivoNaoVenda.set("LojId", LojId);
 		viewModelNaoVenda.motivoNaoVenda.set("MnvId", parseInt(viewModelNaoVenda.motivos[0].MnvId));
         
-		viewModelNaoVenda.dsNaoVenda.sync(); 	                
+        console.log("Nao Venda preenchido " , viewModelNaoVenda.motivoNaoVenda);
         
-		//Marca o checkbox como default
-		document.getElementById("chkVendeu").checked = "checked";
-        
-		app.navigate("dentroFila-view");
-	}
-			
-	function cancelarNaoVenda() {
-		viewModelNaoVenda.dsNaoVenda.cancelChanges();         
-		document.getElementById("chkVendeu").checked = "checked";
-		app.navigate("#dentroFila-view");                 
-	}
-    
-	function motivosNaoVenda() {	
-        viewModelNaoVenda.set("confirma", false);  
-		dsMotivosNaoVenda.options.transport.read.url = baseUrl + "/RmMotNaoVenda";
-		dsMotivosNaoVenda.read(); 
-		var naoVenda = viewModelNaoVenda.dsNaoVenda.add(); 
-		viewModelNaoVenda.set("motivoNaoVenda", naoVenda); 
-	}
-
-    function onConfirmNaoVenda(button) {
-		viewModelNaoVenda.set("confirma", button);
 	}
     
 	$.extend(window, {
-		showMotivosNaoVenda: motivosNaoVenda,
-		viewModelNaoVenda: viewModelNaoVenda
+		viewModelNaoVenda: viewModelNaoVenda,
+		naoVendaViewInit: naoVendaViewInit,
+		naoVendaViewShow: naoVendaViewShow
 	});
 })(jQuery);

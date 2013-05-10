0c92ebd38e57c5fe055ab9a0939fab4cc1ea90ab
MktOneRm MktOneRm <mktonerm@gmail.com>
2013-05-09 22:27:43 -0500

    ......

--- TopLineApp/scripts/topLineConsultas.js
+++ TopLineApp/scripts/topLineConsultas.js
@@ -1,40 +1,81 @@
 (function($, undefined) {
-
+	var baseUrl = "http://www.revenuemachine.com.br/mobile/api";
+	//var baseUrl = "http://localhost:50000/api";
     
-    var data = [
-		{ Data: "01/01/2003", Previsto:50, Realizado: 10, Percentual: 20},
-		{ Data: "02/01/2003", Previsto:100, Realizado: 10, Percentual: 10},
-		{ Data: "03/01/2003", Previsto:30, Realizado: 15, Percentual: 50}
-	];
-    
-	var scData = {
-		model:{
-			id: "Data",
-			fields: {
-				Data: { type: "string"},
-				Previsto: { type: "int"},  
-                Realizado: { type: "int"},
-				Percentual: { type: "float"}  
+	kendo.data.binders.date = kendo.data.Binder.extend({
+		init: function (element, bindings, options) {
+			kendo.data.Binder.fn.init.call(this, element, bindings, options);
+ 
+			this.dateformat = $(element).data("dateformat");
+		},
+		refresh: function () {
+			var data = this.bindings["date"].get();
+			if (data) {
+				var dateObj = new Date(data);
+				$(this.element).text(kendo.toString(dateObj, this.dateformat, "pt-BR") + " Vendas - Até " +  kendo.toString(new Date(), "HH:mm") + " Hs");
 			}
+		}
+	});
+    
+	//Schema Atendimento
+	var scDesRealizadoCol = { 
+		model: {
+			id: "RedeLojaId",
+			fields:{
+				RedeLojaId: { editable: false, nullable: false },
+				LojaId: { editable: false, nullable: false },
+				LojaColId: { editable: false, nullable: false },
+				HfuId: { editable: false, nullable: false },
+				MetaData: { editable: false, nullable: false },
+				MetaHora: { editable: false, nullable: false },
+				MetaRealizadoVlVenda: { editable: false, nullable: false },
+				MetaRealizadoQtPecas: { editable: false, nullable: false },
+				DataInicialMeta: { editable: false, nullable: false },
+				DataFinalMeta: { editable: false, nullable: false },
+				MetaRealizadoAcumulado: { editable: false, nullable: false }
+			} 
 		}
 	};
     
-	var dsData = new kendo.data.DataSource({ 
-		data: data,
-		schema: scData,
-		change: function (e) {			
-			viewModelConsultas.set("dataSelecionado", this.view());
-		}
+	//dataSource Atendimento
+	var dsDesRealizadoCol = new kendo.data.DataSource({                    
+		transport: {						
+			read:  {
+				url: baseUrl + "/RmDesempenho",							
+				type:"GET"      
+				,contentType: "application/json"
+				,dataType: "json"
+			},
+			parameterMap: function(data, operation) {
+				if (operation == "read") {
+					return {
+						lojaId: 1,
+						hfuId: 1, 
+						lojaColId: 2,
+						noDia: false, 
+						ateData: false
+					}
+				}
+				else if (operation !== "read" && data.models) {							
+					return kendo.stringify(data.models[0]);
+				}
+			}
+		},
+		batch: true,
+		schema: scDesRealizadoCol,
 	});
     
-    var viewModelConsultas = kendo.observable({	
-        dsData: dsData,
-        dataSelecionado: []
-        
-    })
+	var viewModelConsultas = kendo.observable({	
+		dsDesRealizadoCol: dsDesRealizadoCol,
+		dataAtual: ""
+	})
+    
+	function desRealizadoColShow() {	
+		viewModelConsultas.set("dataAtual", new Date());
+	}
     
 	$.extend(window, {
-        viewModelConsultas: viewModelConsultas
+		viewModelConsultas: viewModelConsultas,
+        desRealizadoColShow: desRealizadoColShow
 	});
-    
 })(jQuery);
